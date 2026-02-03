bcfc6d76bf56fc73f65bbae1a381a7d6dfbb1b22
Andrew Alba <andrew.alba@gmail.com>
2026-02-03 00:42:54 -0600

    style(Navigation.vue): format button and svg elements for improved readability and maintainability fix(Navigation.vue): remove redundant class "hidden" from navbar div for correct display behavior

--- app/app/components/common/Navigation.vue
+++ app/app/components/common/Navigation.vue
@@ -35,11 +35,19 @@ const logoSrc = computed(() => isDarkMode.value ? darkLogo : lightLogo)
         <img :src="logoSrc" class="h-8" :alt="site.title + 'logo'"/>
         <h3 class="self-center text-2xl font-semibold whitespace-nowrap text-primary dark:text-white">{{ site.title }}</h3>
       </NuxtLink>
-      <button @click="toggleNav" data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-default" aria-expanded="false">
+      <button
+          @click="toggleNav"
+          data-collapse-toggle="navbar-default"
+          type="button"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary"
+          aria-controls="navbar-default"
+          aria-expanded="false">
         <span class="sr-only">Open main menu</span>
-        <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
+        <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/>
+        </svg>
       </button>
-      <div :class="showMenu ? 'flex' : 'hidden'" class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <div :class="showMenu ? 'flex' : 'hidden'" class="w-full md:block md:w-auto" id="navbar-default">
         <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
           <template v-for="link in site.links">
             <li>
